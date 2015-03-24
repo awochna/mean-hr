7a26e6233501eacd352dfc620256e28cc51f16cf
Alex Wochna <awochna@email.arizona.edu>
2015-03-24 13:56:40 -0700

    Add primitive routing for the employee resource (GET requests only) and static files

--- index.js
+++ index.js
@@ -7,8 +7,27 @@ http.createServer(function (req, res) {
   // In case the client uses lower-case for methods.
   req.method = req.method.toUpperCase();
   console.log(req.method + ' ' + req.url);
-  res.end('The current time is ' + Date.now())
 
+  if (req.method !== 'GET') {
+    res.writeHead(501, {
+      'Content-Type': 'text/plain'
+    });
+    return res.end(req.method + ' is not implemented by this server.');
+  }
+
+  if (_url = /^\/employees$/i.exec(req.url)) {
+    // Return a list of valid employees.
+    res.writeHead(200);
+    return res.end('employee list');
+  } else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)) {
+    // Find the employee by the id in the route.
+    res.writeHead(200);
+    return res.end('a single employee');
+  } else {
+    // Try to send the static file.
+    res.writeHead(200);
+    return res.end('static file maybe');
+  }
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
