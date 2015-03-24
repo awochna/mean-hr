118c1113d673dbca1f5dcbc7fc58e4367af6e8d4
Alex Wochna <awochna@email.arizona.edu>
2015-03-24 14:51:41 -0700

    Use the new responder in the index.js file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var employeeService = require('./lib/employees');
+var responder = require('./lib/responseGenerator');
+var staticFile = responder.staticFile('/public');
 
 http.createServer(function (req, res) {
   // A parsed URL to work with in case there are parameters.
@@ -21,27 +23,31 @@ http.createServer(function (req, res) {
     empolyeeService.getEmployees(function (error, data) {
       if (error) {
         // Send a 500 error.
-        res.writeHead(500);
+        return responder.send500(error, res);
       }
       // Send the data with a 200 status code.
+      return responder.sendJson(data, res);
     });
   } else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)) {
     // Find the employee by the id in the route.
     employeeService.getEmployee(_url[1], function (error, data) {
       if (error) {
         // Send a 500 error.
-        res.writeHead(500);
+        return responder.send500(error, res);
       }
 
       if (!data) {
         // Send a 404 error.
-        res.writeHead(404);
+        return responder.send404(res);
       }
 
       // Send the data with a 200 status code.
+      return responder.sendJson(data, res);
     });
   } else {
     // Try to send the static file if it exists, if not, send a 404.
+    res.writeHead(200);
+    res.end('static file maybe');
   }
 }).listen(1337, '127.0.0.1');
 
